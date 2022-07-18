18390382383e77e22e956665d9f0e15aeb0218f0
Vitaliy-Zinevich <everwoods21011993@gmail.com>
2022-07-19 00:28:20 +0300

    PersonalSlider

--- src/components/PersonalSlider.tsx
+++ src/components/PersonalSlider.tsx
@@ -1,26 +1,37 @@
-import love3 from "../assets/slider/love3.jpg"
-import lily1 from "../assets/slider/lily1.jpg"
+import {motion}  from 'framer-motion';
+import {useRef, useState, useEffect} from 'react';
+import images from "./imagesPersonal";
 
-const PersonalSlider = ( {}) => {
+const PersonalSlider = () => {
+    const [width, setWidth] = useState(0);
+    const carousel = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+    useEffect(() => {
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }, []);
+ 
+
     return (
-        <div className="main-container">
-         <div className="window">
-           <div className="sliderBlock">
-              <div className="personalSlider"><img src={love3}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-              <div className="personalSlider"><img src={lily1}/></div>
-            </div>
-          </div>
-        </div>
+      <div className="sliderBlock">
+        <motion.div ref={carousel} className="carousel">
+            <motion.div 
+              drag="x" 
+              dragConstraints={{right: 0, left: -width}}
+              className="inner-carousel"
+            >
+              {images.map ((image) => {
+                 return (
+                   <motion.div key={image} className="personalSlider" >   
+                     <img src={image}  alt=""/>
+                   </motion.div>    
+                 );
+              })}
+            </motion.div>
+        </motion.div>
+     </div>
     )
 }
 
 export default PersonalSlider ;
+
+
